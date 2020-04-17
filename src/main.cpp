88575c33809d22d81048873651928f1c20e5b86e
unknown <medvedev.danylagmail.comgit config --global user.email medvedev.danylagmail.com>
2020-04-17 22:11:04 +0300

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
-
+#include "main.h"
 int leftButtonPin = 8;
 int rightButtonPin = 7;
 
@@ -8,34 +8,10 @@ Servo servo;
 int servoPin=9;
 int step_time=500;
 int step_deg = 10; // angle of rotatiom
-int numbMicroStep =10; //for smooth rotation 
+int numbMicroStep =10; //for smooth rotation
 
 int serva_position;
 
-        int clockwiseMove(){
-        servo.attach(servoPin); //Servo in on
-         for (int x = 0; x < numbMicroStep; x++){
-          serva_position = serva_position+step_deg/numbMicroStep;
-          servo.write(serva_position);
-          delay(step_time/numbMicroStep);
-         }      
-
-        servo.detach(); //Servo is off
-        return serva_position;
-        }
-
-        int counter_clockwiseMove(){
-          servo.attach(servoPin);
-            for (int x = 0; x < numbMicroStep; x++){
-              serva_position = serva_position-step_deg/numbMicroStep;
-              servo.write(serva_position);
-              delay(step_time/numbMicroStep);
-         }      
-
-        servo.detach(); 
-        return serva_position;
-        }
- 
 void setup() 
 {
 
@@ -48,6 +24,29 @@ void loop()
       }
  if ((digitalRead(rightButtonPin)==HIGH)&&serva_position!=0) 
       { 
-          serva_position = counter_clockwiseMove();
+          serva_position = counterClockwiseMove();
       }
 }
+int clockwiseMove(){
+        servo.attach(servoPin); //Servo in on
+         for (int x = 0; x < numbMicroStep; x++){
+          serva_position = serva_position+step_deg/numbMicroStep;
+          servo.write(serva_position);
+          delay(step_time/numbMicroStep);
+         }      
+
+        servo.detach(); //Servo is off
+        return serva_position;
+        }
+
+int counterClockwiseMove(){
+          servo.attach(servoPin);
+            for (int x = 0; x < numbMicroStep; x++){
+              serva_position = serva_position-step_deg/numbMicroStep;
+              servo.write(serva_position);
+              delay(step_time/numbMicroStep);
+         }      
+
+        servo.detach(); 
+        return serva_position;
+        }
---
+++ src/main.h
@@ -0,0 +1,2 @@
+int clockwiseMove();
+int counterClockwiseMove();
