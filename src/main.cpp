8629198c8e3d255dba703085b9b9817acbd68f59
unknown <medvedev.danylagmail.comgit config --global user.email medvedev.danylagmail.com>
2020-04-17 17:26:48 +0300

    detaching serva after movement for remove noise

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,27 @@ int step_deg = 10;
 int serva_position = 0;
 void setup() 
 {
- servo.attach(servoPin);
+ servo.attach(servoPin); //Сервопривод подключается 
  servo.write(serva_position);
  pinMode(leftButtonPin, INPUT); 
 }
 void loop() 
 {
    if ((digitalRead(leftButtonPin)==HIGH)&&serva_position!=180) 
-      {      
+      { 
+        servo.attach(servoPin); //Сервопривод подключается     
         serva_position = serva_position+step_deg;
         servo.write(serva_position);
         delay(step_time);
+        servo.detach(); //Сервопривод отключается
       }
  if ((digitalRead(rightButtonPin)==HIGH)) 
-      {      
+      { 
+        servo.attach(servoPin); //Сервопривод подключается     
         serva_position =0;
         servo.write(serva_position);
         delay(500);
+        servo.detach(); //Сервопривод отключается
       }
 }
  
